0e9a02f9e0f5981f0b42bd10546096bb9caf6451
Polina Popova <popovapolina2133@gmail.com>
2023-10-17 21:14:22 +0300

    Create class Group

---
+++ src/Task1/Group.java
@@ -0,0 +1,11 @@
+package Task1;
+
+public class Group {
+    public int group1;
+    public int group2;
+
+    public Group(int group1, int group2) {
+        this.group1 = group1;
+        this.group2 = group2;
+    }
+}
