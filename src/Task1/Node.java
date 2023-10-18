30ddde82e19133f8c344b282d6bb11ad977eebb0
Polina Popova <popovapolina2133@gmail.com>
2023-10-18 21:36:55 +0300

    update

--- src/Task1/Node.java
+++ src/Task1/Node.java
@@ -7,25 +7,29 @@ public abstract class Node<T extends Rectanglable> {
     protected Rectangle boundBox;
     public List<T> rectanglables;
 
-    public Node(Rectangle boundBox) {
+    public Node() {
         this.boundBox = null;
         this.rectanglables = new ArrayList<>();
     }
 
     public void addRectanglable(T rectanglable) {
+        if (boundBox == null) {
+            boundBox = rectanglable.getRectangle();
+            return;
+        }
         rectanglables.add(rectanglable);
-        for (T lala : rectanglables) {
-            Rectangle rect = lala.getRectangle();
-            if (rect.intersects(boundBox)) {
-                boundBox = boundBox.combine(boundBox, rect);
-            }
+        Rectangle rect = rectanglable.getRectangle();
+        if (rect.intersects(boundBox)) {
+            boundBox = boundBox.combine(boundBox, rect);
         }
     }
+
     public Rectangle getBoundBox() {
         return boundBox;
     }
 
     abstract boolean isLeaf();
+
     abstract int size();
 
     public void clear() {
