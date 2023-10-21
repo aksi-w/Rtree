e68136031dd7519c27241fc0e043f4fc3fc71038
Polina Popova <popovapolina2133@gmail.com>
2023-10-21 20:39:44 +0300

    add print

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,14 +1,24 @@
 package Task1;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle1 = new Rectangle(1, 1, 4, 4);
-        Rectangle rectangle2 = new Rectangle(2, 2, 6, 6);
+        Insert insert = new Insert();
 
-        double difference = rectangle1.calculateDifference(rectangle2);
-        System.out.println("Разница площадей: " + difference);
+        Rectanglable rect1 = new Rectangle(0, 0, 2, 2);
+        Rectanglable rect2 = new Rectangle(1, 1, 3, 3);
+        Rectanglable rect3 = new Rectangle(4, 4, 6, 6);
+        Rectanglable rect4 = new Rectangle(5, 5, 7, 7);
+        Rectanglable rect5 = new Rectangle(8, 8, 10, 10);
 
+        insert.insert(rect1);
+        insert.insert(rect2);
+        insert.insert(rect3);
+        insert.insert(rect4);
+        insert.insert(rect5);
+
+
+        for (Rectanglable rectanglable : insert.root.rectanglables) {
+            System.out.println(rectanglable.getRectangle());
+        }
     }
 }
