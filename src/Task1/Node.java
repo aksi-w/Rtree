b552a7cb8d51ad06b88e6a939c9dcea42a4d4bb5
Polina Popova <popovapolina2133@gmail.com>
2023-10-24 16:24:44 +0300

    update

--- src/Task1/Node.java
+++ src/Task1/Node.java
@@ -3,9 +3,21 @@ package Task1;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Node<T extends Rectanglable> {
+public class Node<T extends Rectanglable> implements Rectanglable {
     protected Rectangle boundBox;
-    public List<T> rectanglables;
+
+    private List<T> rectanglables;
+
+    private List<Node<T>> child;
+
+    public List<T> value() {
+        return rectanglables;
+    }
+
+    public List<Node<T>> getChild() {
+        return child;
+    }
+
 
     public Node() {
         this.boundBox = null;
@@ -13,15 +25,20 @@ public class Node<T extends Rectanglable> {
     }
 
     public void addRectanglable(T rectanglable) {
-        if (boundBox == null) {
-            boundBox = rectanglable.getRectangle();
-            return;
-        }
+
+        /*
         rectanglables.add(rectanglable);
         Rectangle rect = rectanglable.getRectangle();
         if (rect.intersects(boundBox)) {
             boundBox = boundBox.combine(boundBox, rect);
+        }*/
+        if (boundBox == null) {
+            boundBox = rectanglable.getRectangle();
         }
+    }
+
+    public void addChild(T child){
+
     }
 
     public Rectangle getBoundBox() {
@@ -35,11 +52,15 @@ public class Node<T extends Rectanglable> {
     public void clear() {
         rectanglables.clear();
         boundBox = null;
+
     }
 
     int size() {
         return rectanglables.size();
     }
 
-
+    @Override
+    public Rectangle getRectangle() {
+        return getBoundBox();
+    }
 }
