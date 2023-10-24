9cffe65e1aa07848a56b0d5ce89cb54e6a1bd25d
Polina Popova <popovapolina2133@gmail.com>
2023-10-24 16:25:33 +0300

    add new method 'enlargement'

--- src/Task1/Rectangle.java
+++ src/Task1/Rectangle.java
@@ -1,8 +1,7 @@
 package Task1;
 
-public class Rectangle implements Rectanglable{
+public class Rectangle implements Rectanglable {
     private final double x1, y1, x2, y2;
-    private double minDifference = 100000000;
 
     public Rectangle(double x1, double y1, double x2, double y2) {
         this.x1 = Math.min(x1, x2);
@@ -10,8 +9,6 @@ public class Rectangle implements Rectanglable{
         this.x2 = Math.max(x1, x2);
         this.y2 = Math.max(y1, y2);
     }
-
-
 
     public boolean intersects(Rectangle otherRectangle) {
         return x1 < otherRectangle.x2 && x2 > otherRectangle.x1 && y1 < otherRectangle.y2 && y2 > otherRectangle.y1;
@@ -27,17 +24,25 @@ public class Rectangle implements Rectanglable{
         return Math.abs(currentSquare - otherSquare);
     }
 
+    public double enlargement(Rectangle other) {
+        return combine(this, other).square() - this.square();
+    }
+
     public static Rectangle combine(Rectangle rect1, Rectangle rect2) {
         double minX = Math.min(rect1.x1, rect2.x1);
         double minY = Math.min(rect1.y1, rect2.y1);
         double maxX = Math.max(rect1.x2, rect2.x2);
         double maxY = Math.max(rect1.y2, rect2.y2);
-        
+
         return new Rectangle(minX, minY, maxX, maxY);
     }
 
     @Override
     public Rectangle getRectangle() {
         return this;
+    }
+
+    public String toString() {
+        return "Rectangle [x1=" + x1 + ", y1=" + y1 + ", x2=" + x2 + ", y2=" + y2 + "]";
     }
 }
